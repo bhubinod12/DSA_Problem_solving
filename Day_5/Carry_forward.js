b441fa14de307d2b64368139ca73beebedbc931a
Binod Kumar Yadav <binod.yadav@motorolasolutions.com>
2023-08-06 20:38:27 +0530

    Added all other questions:

--- Day_5/Carry_forward.js
+++ Day_5/Carry_forward.js
@@ -68,7 +68,7 @@
 //  5 - 2 +1 = 4
 //  (s , e) => e-s+1;
 
-let arr = [1,6,2,3,1,3,4,6,4,6,3];
+// let arr = [1,6,2,3,1,3,4,6,4,6,3];
 // min = 1
 // max = 6
 // [1,6] 
@@ -103,7 +103,8 @@ let arr = [1,6,2,3,1,3,4,6,4,6,3];
 //                 }
 //                 // ans = Math.min(ans,i-lastMaxIndex+1);
 //             }
-//         } if (arr[i] == max) {
+//         } 
+//            if (arr[i] == max) {
 //             lastMaxIndex = i;
 //             if (lastMinIndex >= 0) {
 //                 if (ans > i-lastMinIndex+1) {
@@ -116,6 +117,23 @@ let arr = [1,6,2,3,1,3,4,6,4,6,3];
 //     return ans;
 // }
 // console.log(minLenSubArray(arr));
+// Q3: Given an integer array A containing N distinct integers, you have to find all the leaders in array A. 
+// An element is a leader if it is strictly greater than all the elements to its left side.
+ let A = [2,5,3,4,17,16];
+//  2 -> leader
+//  5 -> leader
+//  3 -> no
+//  17 -> leader
+//  A[i] > (0, i-1)
+// ==
+// 2 5 3 4 17 16
+
+// max 2  5 17
+// ans 1  2 3
+// ans = 3
+
+// 
+
 
 
 
